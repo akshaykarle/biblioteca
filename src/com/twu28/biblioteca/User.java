598a0f747a4c28b26155e64236b03929e9d8051c
Akshay Karle <akshay.a.karle@gmail.com>
2012-09-21 00:14:55 +0530

    Add user class.

---
+++ src/com/twu28/biblioteca/User.java
@@ -0,0 +1,13 @@
+package com.twu28.biblioteca;
+
+class User {
+    private String userName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public boolean authenticate(String usernameToAuthenticate, String passwordToValidate) {
+        return false;
+    }
+}
